6de02af13724421686e11885a387970997e38078
Jon Taylor <jonathan.1.taylor@gov.bc.ca>
2019-05-13 11:37:52 -0700

    check in code for cron job

---
+++ openshift/job-scheduling/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace job_scheduling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
